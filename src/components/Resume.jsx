a82d042e3699c153668e54630274f1690fdf4f95
Jared Rothenberg <jaredar@gmail.com>
2020-05-30 22:55:56 -0700

    update resume link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -112,7 +112,7 @@ export default class Resume extends Component {
             data-sal-duration="500"
           >
             <a
-              href="https://jaredsportfolio.s3-us-west-1.amazonaws.com/Jared+Rothenberg's+Resume.pdf"
+              href="https://jared-rothenbergs-portfolio.s3-us-west-1.amazonaws.com/Jared+Rothenberg's+Resume.pdf"
               target="_blank"
               className="resume-button"
               rel="noreferrer"
